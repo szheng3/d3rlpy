4cd09604c7777c8043aa832c34de978e1a3dbd42
Sept <sszheng3@gmail.com>
2023-03-23 19:10:45 -0400

    avg

--- d3rlpy/metrics/scorer.py
+++ d3rlpy/metrics/scorer.py
@@ -126,8 +126,10 @@ def average_true_q_value_scorer(algo: AlgoProtocol, episodes: List[Episode]) -> 
     for episode in episodes:
         for batch in _make_batches(episode, WINDOW_SIZE, algo.n_frames):
             # estimate values for next observations
-            next_actions = algo.predict([batch.next_observations[0]])
-            next_values = algo.predict_value([batch.next_observations[0]], next_actions)
+            next_actions = algo.predict(batch.next_observations)
+            next_values = algo.predict_value(
+                batch.next_observations, next_actions
+            )
             mask = (1.0 - np.asarray(batch.terminals)).reshape(-1)
             rewards = np.asarray(batch.rewards).reshape(-1)
             if algo.reward_scaler:
