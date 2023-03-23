022754ea98c9a49d1bbf93bece01c5a2711eae80
Sept <sszheng3@gmail.com>
2023-03-23 15:35:59 -0400

    avg

--- d3rlpy/metrics/scorer.py
+++ d3rlpy/metrics/scorer.py
@@ -113,7 +113,7 @@ def td_error_scorer(algo: AlgoProtocol, episodes: List[Episode]) -> float:
     return float(np.mean(total_errors))
 
 
-def true_q_value_scorer(algo: AlgoProtocol, episodes: List[Episode]) -> float:
+def average_true_q_value_scorer(algo: AlgoProtocol, episodes: List[Episode]) -> float:
     """
     Computes the total true discount reward value of action a in initial state s.
     Args:
@@ -512,80 +512,6 @@ def evaluate_on_environment(
 
     return scorer
 
-#
-# def evaluate_on_environment(
-#     env: gym.Env, n_trials: int = 10, epsilon: float = 0.0, render: bool = False
-# ) -> Callable[..., float]:
-#     """Returns scorer function of evaluation on environment.
-#     This function returns scorer function, which is suitable to the standard
-#     scikit-learn scorer function style.
-#     The metrics of the scorer function is ideal metrics to evaluate the
-#     resulted policies.
-#     .. code-block:: python
-#         import gym
-#         from d3rlpy.algos import DQN
-#         from d3rlpy.metrics.scorer import evaluate_on_environment
-#         env = gym.make('CartPole-v0')
-#         scorer = evaluate_on_environment(env)
-#         cql = CQL()
-#         mean_episode_return = scorer(cql)
-#     Args:
-#         env: gym-styled environment.
-#         n_trials: the number of trials.
-#         epsilon: noise factor for epsilon-greedy policy.
-#         render: flag to render environment.
-#     Returns:
-#         scoerer function.
-#     """
-#
-#     # for image observation
-#     observation_shape = env.observation_space.shape
-#     is_image = len(observation_shape) == 3
-#
-#     def scorer(algo: AlgoProtocol, *args: Any) -> float:
-#         if is_image:
-#             stacked_observation = StackedObservation(
-#                 observation_shape, algo.n_frames
-#             )
-#
-#         episode_rewards = []
-#         first = []
-#         for _ in range(n_trials):
-#             observation = env.reset()
-#             episode_reward = 0.0
-#
-#             # frame stacking
-#             if is_image:
-#                 stacked_observation.clear()
-#                 stacked_observation.append(observation)
-#             count = 0
-#             while True:
-#                 # take action
-#                 if np.random.random() < epsilon:
-#                     action = env.action_space.sample()
-#                 else:
-#                     if is_image:
-#                         action = algo.predict([stacked_observation.eval()])[0]
-#                     else:
-#                         action = algo.predict([observation])[0]
-#
-#                 observation, reward, done, _ = env.step(action)
-#                 episode_reward += reward * algo.gamma ** count
-#                 if count == 0:
-#                     first.append(algo.predict_value([observation], [action]))
-#                 if is_image:
-#                     stacked_observation.append(observation)
-#
-#                 if render:
-#                     env.render()
-#
-#                 if done:
-#                     break
-#                 count += 1
-#             episode_rewards.append(episode_reward)
-#         return float(np.mean(episode_rewards)), float(np.mean(first))
-#
-#     return scorer
 
 
 def dynamics_observation_prediction_error_scorer(
